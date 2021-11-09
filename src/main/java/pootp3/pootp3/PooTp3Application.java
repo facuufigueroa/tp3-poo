7973c933dfa8b7b1e831bc2537cede2bac2fb9c9
facuufigueroa <figueroafacundo123@gmail.com>
2021-11-09 20:04:50 -0300

    primer commit

---
+++ src/main/java/pootp3/pootp3/PooTp3Application.java
@@ -0,0 +1,13 @@
+package pootp3.pootp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooTp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pootp3/pootp3/PooTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pootp3.pootp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
